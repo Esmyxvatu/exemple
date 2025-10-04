173e4c480dfc4547624c490c01f83930ec824d80
Esmyxvatu <138006708+Esmyxvatu@users.noreply.github.com>
2025-10-04 13:02:58 +0200

    Create python/a.py

---
+++ python/a.py
@@ -0,0 +1,11 @@
+products = [
+ {"name": "apple", "price": 1.25},
+ {"name": "pear", "price": 2.3},
+ {"name": "banana", "price": 1.25},
+ {"name": "orange", "price": 2},
+ {"name": "kiwi", "price": 1.25}
+]
+
+price = 1.25
+
+# Trouve et écris dans la console tout les produits qui coûte price
