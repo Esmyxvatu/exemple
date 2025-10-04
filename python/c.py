7821093bdb71d0d863dec1d7713117f899bb3f87
Esmyxvatu <138006708+Esmyxvatu@users.noreply.github.com>
2025-10-04 13:07:00 +0200

    Create c.py

---
+++ python/c.py
@@ -0,0 +1,12 @@
+users = {
+  "esmyx": "pswd",
+  "maxow": "lol",
+  "mantis": "skibidi",
+  "rasax": "niger"
+}
+
+username = input("username: ")
+# demande le mot de passe
+
+for name in users.key:
+  # vérifie si le nom fourni correspond à celui là puis vérifie si le mdp correspond oublie pas les messages 
